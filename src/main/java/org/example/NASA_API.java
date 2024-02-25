fc7e8ea34182beaebcd8155dd81491d2c7c886d5
Sergey Ivanov <1539ivanov@mail.ru>
2024-02-25 20:04:52 +0300

    Redone

--- src/main/java/org/example/NASA_API.java
+++ src/main/java/org/example/NASA_API.java
@@ -8,10 +8,10 @@ import org.apache.http.client.methods.HttpGet;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClientBuilder;
 
-import java.io.BufferedReader;
-import java.io.FileWriter;
+import java.io.File;
+import java.io.FileOutputStream;
 import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.OutputStream;
 
 //Задание 2 "Чтение данных API NASA"
 public class NASA_API {
@@ -33,21 +33,14 @@ public class NASA_API {
                         TypeReference<NASAServiceResponse>() {
                         });
 
-        HttpGet request2 = new HttpGet(NASAResponse.getUrl());
+        HttpGet request2 = new HttpGet(NASAResponse.getHdurl());
         CloseableHttpResponse response2 = httpClient.execute(request2);
 
         String fileName = NASAResponse.getUrl().split("/")[NASAResponse.getUrl().split("/").length - 1];
 
-        try {
-            FileWriter fileWriter = new FileWriter(fileName);
-            BufferedReader fileReader = new BufferedReader(new InputStreamReader(response2.getEntity().getContent()));
-            while (fileReader.ready()) {
-                fileWriter.write(fileReader.read());
-            }
-            fileWriter.close();
-            fileReader.close();
-        } catch (Exception e) {
-            e.getStackTrace();
+        File file = new File(fileName);
+        try (OutputStream writer = new FileOutputStream(file)) {
+            writer.write(response2.getEntity().getContent().readAllBytes());
         }
     }
 }
